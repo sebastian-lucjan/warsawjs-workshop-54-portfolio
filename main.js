0a0ca8fd99e1af405c95efef3c6c89fa2833a3c4
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-06-22 15:58:36 +0200

    Base config - eslint, prettier

---
+++ main.js
@@ -0,0 +1,11 @@
+const addP2 = (x, y) => x + y;
+// const addP3 = (x, y) => x + y;
+// const addP4 = (x, y) => x + y;
+const add = (x, y) => x + y;
+console.log(add(5, 3));
+
+const animalsArray = ['dog', 'cat', 'tiger'];
+
+console.log(animalsArray.indexOf('tiger'));
+
+addP2(3, 2);
